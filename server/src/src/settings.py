40775f8d10db0cc146a296e81acfd38a7d558db6
Dain Jeong <dainjeong1004@gmail.com>
2024-04-08 18:15:59 +0900

    database -> postgresql

--- server/src/src/settings.py
+++ server/src/src/settings.py
@@ -76,10 +76,14 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'captain',
+        'PASSWORD': '0000',
+        'HOST': 'localhost',
+        'PORT': '',
+	    }
 }
 
 
