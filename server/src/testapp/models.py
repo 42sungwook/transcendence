79ef555b4a6173694e54a31b52713ea696f036c8
Dain Jeong <dainjeong1004@gmail.com>
2024-04-08 20:57:30 +0900

    [Feat] Create user page

--- server/src/testapp/models.py
+++ server/src/testapp/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class TestModel(models.Model):
+class User(models.Model):
+	intra_id = models.CharField(max_length=255)
 	name = models.CharField(max_length=255)
-	age = models.IntegerField()
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-
-	def __str__(self):
-		return self.name
--- server/src/testapp/views.py
+++ server/src/testapp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'testapp/home.html')
 
+def user(request):
+    return render(request, 'testapp/user.html')
